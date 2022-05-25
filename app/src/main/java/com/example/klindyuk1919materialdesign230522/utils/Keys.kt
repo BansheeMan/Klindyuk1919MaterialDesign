b2f3a9ebbf150781a45c2e5b68077b3008acc9c8
Banshee <klindyuk98@gmail.com>
2022-05-26 01:52:28 +0300

    HW 1 Добавил и настроил BottomSheetBehavior, поиск по WIKI

--- app/src/main/java/com/example/klindyuk1919materialdesign230522/utils/Keys.kt
+++ app/src/main/java/com/example/klindyuk1919materialdesign230522/utils/Keys.kt
@@ -2,3 +2,4 @@ package com.example.klindyuk1919materialdesign230522.utils
 
 const val NASA_DOMAIN = "https://api.nasa.gov/"
 const val NASA_ENDPOINT = "planetary/apod"
+const val WIKI_DOMAIN = "https://en.wikipedia.org/wiki/"
--- app/src/main/java/com/example/klindyuk1919materialdesign230522/view_viewmodel/picture/PictureOfTheDayFragment.kt
+++ app/src/main/java/com/example/klindyuk1919materialdesign230522/view_viewmodel/picture/PictureOfTheDayFragment.kt
@@ -1,6 +1,9 @@
 package com.example.klindyuk1919materialdesign230522.view_viewmodel.picture
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,7 +13,9 @@ import coil.load
 import coil.transform.CircleCropTransformation
 import com.example.klindyuk1919materialdesign230522.R
 import com.example.klindyuk1919materialdesign230522.databinding.FragmentPictureOfTheDayBinding
+import com.example.klindyuk1919materialdesign230522.utils.WIKI_DOMAIN
 import com.example.klindyuk1919materialdesign230522.utils.showSnackBar
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class PictureOfTheDayFragment : Fragment() {
 
@@ -37,6 +42,39 @@ class PictureOfTheDayFragment : Fragment() {
         }
         //TODO уточнить у Андрея; можно ли передавать context по MVVM;
         viewModel.sendRequest(requireContext())
+        findWiki()
+        stateBottomSheetL()
+    }
+
+    private fun stateBottomSheetL() {
+        val bottomSheetBehavior = BottomSheetBehavior.from(binding.hackBsl.bottomSheetContainer)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED //берет стартовую точку от app:behavior_peekHeight в XML
+        bottomSheetBehavior.addBottomSheetCallback( object : BottomSheetBehavior.BottomSheetCallback(){
+            //ЗАКЛАДКИ!!!
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                /*when(newState){
+                    BottomSheetBehavior.STATE_DRAGGING ->{}
+                    BottomSheetBehavior.STATE_COLLAPSED -> {}
+                    BottomSheetBehavior.STATE_EXPANDED -> {}
+                    BottomSheetBehavior.STATE_HALF_EXPANDED -> {}
+                    BottomSheetBehavior.STATE_HIDDEN -> {}
+                    BottomSheetBehavior.STATE_SETTLING -> {}
+                }*/
+            }
+            //ЗАКЛАДКИ!!!
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                //Log.d("@@@","$slideOffset")
+            }
+
+        })
+    }
+
+    private fun findWiki() {
+        binding.inputLayout.setEndIconOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse("$WIKI_DOMAIN${binding.inputEditText.text.toString()}")
+            })
+        }
     }
 
     private fun renderData(appState: PictureOfTheDayAppState) {
@@ -53,17 +91,25 @@ class PictureOfTheDayFragment : Fragment() {
                 }
             }
             is PictureOfTheDayAppState.Loading -> {
-                binding.loadingView.visibility = View.VISIBLE
+                with(binding) {
+                    errorView.visibility = View.GONE
+                    successView.visibility = View.GONE
+                    loadingView.visibility = View.VISIBLE
+                }
             }
             is PictureOfTheDayAppState.Success -> {
                 with(binding) {
+                    errorView.visibility = View.GONE
                     loadingView.visibility = View.GONE
                     successView.visibility = View.VISIBLE
                     imageView.load(appState.pictureOfTheDayResponseData.url) {
-                        placeholder(R.drawable.loading)
+                        placeholder(R.drawable.welcome)   //TODO взависимости от картинки placeholder меняет выводимый размер основной картинки
                         transformations(CircleCropTransformation())
                         crossfade(2000) //плавный переход, изменение непрозрачности
                     }
+                    hackBsl.title.text = appState.pictureOfTheDayResponseData.title
+                    hackBsl.explanation.text = appState.pictureOfTheDayResponseData.explanation
+
                 }
             }
         }
