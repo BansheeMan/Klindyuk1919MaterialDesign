92233f2ae128b6f579f595c5d64716a60b06d9ac
Banshee <klindyuk98@gmail.com>
2022-06-10 18:44:39 +0300

    HW3 Связал ViewPager c TabLayout

--- app/src/main/java/com/example/klindyuk1919materialdesign230522/view_viewmodel/les3/Les3VPFragment.kt
+++ app/src/main/java/com/example/klindyuk1919materialdesign230522/view_viewmodel/les3/Les3VPFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.klindyuk1919materialdesign230522.R
 import com.example.klindyuk1919materialdesign230522.databinding.FragmentLes3VPBinding
 
 
@@ -23,7 +24,14 @@ class Les3VPFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.viewPager.adapter = ViewPagerAdapter(requireActivity().supportFragmentManager)
+        binding.viewPager.adapter = ViewPagerAdapter(
+            requireActivity().supportFragmentManager,
+            resources
+        ) //TODO Законно ли это?
+        binding.tabLayout.setupWithViewPager(binding.viewPager)
+        binding.tabLayout.getTabAt(0)?.setIcon(R.drawable.ic_earth)
+        binding.tabLayout.getTabAt(1)?.setIcon(R.drawable.ic_system)
+        binding.tabLayout.getTabAt(2)?.setIcon(R.drawable.ic_mars)
     }
 
     companion object {
@@ -35,5 +43,4 @@ class Les3VPFragment : Fragment() {
         _binding = null
         super.onDestroy()
     }
-
 }
--- app/src/main/java/com/example/klindyuk1919materialdesign230522/view_viewmodel/les3/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/klindyuk1919materialdesign230522/view_viewmodel/les3/ViewPagerAdapter.kt
@@ -1,14 +1,17 @@
 package com.example.klindyuk1919materialdesign230522.view_viewmodel.les3
 
+import android.content.res.Resources
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
+import com.example.klindyuk1919materialdesign230522.R
 import com.example.klindyuk1919materialdesign230522.utils.EARTH
 import com.example.klindyuk1919materialdesign230522.utils.MARS
 import com.example.klindyuk1919materialdesign230522.utils.SOLAR_SYSTEM
 import com.example.klindyuk1919materialdesign230522.view_viewmodel.EarthFragment
 
-class ViewPagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
+class ViewPagerAdapter(fm: FragmentManager, val resources: Resources) :
+    FragmentStatePagerAdapter(fm) {
 
     private val fragments = arrayOf(
         SystemFragment.newInstance(),
@@ -31,5 +34,12 @@ class ViewPagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
         }
     }
 
-
+    override fun getPageTitle(position: Int): CharSequence {
+        return when (position) {
+            EARTH -> resources.getString(R.string.earth)
+            SOLAR_SYSTEM -> resources.getString(R.string.solar_system)
+            MARS -> resources.getString(R.string.mars)
+            else -> resources.getString(R.string.earth)
+        }
+    }
 }
